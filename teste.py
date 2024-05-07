a2713758b895be812c5f67ee380645162767eae0
Rafaella Abreu <168781267+rafaellaabreu@users.noreply.github.com>
2024-05-07 14:36:35 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,12 +1,1 @@
 
-print('Digitar endereço: ')
-rua = input('Logradouro: ')
-num = input('Numero: ')
-cidade = input('Cidade')
-sigla = input('Estado: ')
-
-end = list()
-end.append((rua, num, cidade, sigla))
-
-print(end)
-
